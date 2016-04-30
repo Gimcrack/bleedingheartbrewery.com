00d79548a9e2b9b3bc77a95783453a117acf9f5d
Gimcrack <jeremy.bloomstrom@gmail.com>
2016-04-30 00:48:39 -0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="stylesheet" href="includes/style.css"  />
+<link href='http://fonts.googleapis.com/css?family=Oswald:400,700,300' rel='stylesheet' type='text/css'>
+<meta http-equiv="Content-Type" content="charset=UTF-8;text/html" />
+<title>Bleeding Heart Brewery - Coming Soon to Palmer, AK</title>
+</head>
+
+<body>
+
+<div class="container">
+<div class="header"></div>
+
+<div class="content">
+<h1>Bleeding Heart Brewery</h1>
+<h2>Website coming soon!</h2>
+
+<div style="margin:0 auto; width:150px;">
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="TTE69TLKTE6J4">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+</div>
+
+<p><a href="http://facebook.com/bleedingheartbrewery" target="_blank"><img src="images/fblogo.png" width="100" height="100" align="middle" /></a>
+ In the meantime, find us on <a href="http://facebook.com/bleedingheartbrewery">Facebook</a>. </p>
+</div>
+
+</div>
+
+</body>
+</html>
